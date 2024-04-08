712803e1c4671e6891b1ebda7da9a82a788cda74
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-08 12:15:00 +0900

    ♻️ Docstringsを追加

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    """requestを受け取って、文字列をレンダリングする。
+    Args:
+        request (HttpRequest): HttpRequestオブジェクト
+
+    Returns:
+        HttpResponse: 文字列
+    """
     return HttpResponse("Hello, world. You're at the polls index.")
