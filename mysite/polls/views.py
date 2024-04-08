081cdbba045e96b1651c825a6676d60d5239a5a4
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-08 16:52:21 +0900

    ✨ detail, results, vote関数の追加

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,40 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    """request, question_idを受け取り、文字列を返す
+
+    Args:
+        request (HttpRequest): HttpRequestオブジェクト
+        question_id (int): 質問のid
+
+    Returns:
+        HttpResponse: 文字列
+    """
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    """request, question_idを受け取り、文字列を返す
+
+    Args:
+        request (HttpRequest): HttpRequestオブジェクト
+        question_id (int): 質問のid
+
+    Returns:
+        HttpResponse: 文字列
+    """
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    """request, question_idを受け取り、文字列を返す
+
+    Args:
+        request (HttpRequest): HttpRequestオブジェクト
+        question_id (int): 質問のid
+
+    Returns:
+        HttpResponse: 文字列
+    """
+    return HttpResponse("You're voting on question %s." % question_id)
