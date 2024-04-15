46ff380f82f0629e28484750c2cd5cc92bf5d4b6
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-4.local>
2024-04-15 12:31:41 +0900

    👍 DetailViewについてのテストを追加

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -94,3 +94,21 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             [question2, question1]
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """未来の質問の時、404エラーを返すか確認
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """過去の質問の時の表示を確認
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
